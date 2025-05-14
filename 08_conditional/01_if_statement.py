cb504d2e3288f5b687ecffed170b98733ddac846
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2025-05-14 21:49:34 +0530

    Add files via upload

---
+++ 08_conditional/01_if_statement.py
@@ -0,0 +1,7 @@
+age = 13
+
+if (age>18):
+    print("You can drive")
+    print("Thank you")
+
+print("End the program")
---
+++ 08_conditional/03_if_elif_else.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age : "))
+
+if(age>18):
+    print("you can drive")
+elif(age==18):
+    print("Schedule the interview")
+elif(age==0):
+    print("Hey you just born")
+else:
+    print("Sorry you can not drive")
+
