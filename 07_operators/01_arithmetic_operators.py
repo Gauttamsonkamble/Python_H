0e1cb2d6d5b2c616d92d2b0e9ccc02b52e4345be
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2025-05-14 13:43:56 +0530

    Add files via upload

---
+++ 07_operators/01_arithmetic_operators.py
@@ -0,0 +1,12 @@
+# Arithmetic Operators
+
+a = 30
+b = 10
+
+print( "a + b = ", a + b)
+print( "a - b = ", a - b)
+print( "a * b = ", a * b)
+print( "a / b = ", a / b)
+print( "a % b = ", a % b)
+print( "a // b = ", a // b)
+print( "a ** b = ", a ** b)
---
+++ 07_operators/02_conditional_operators.py
@@ -0,0 +1,17 @@
+
+# Conditional Operators
+
+a = 30
+b = 10
+
+print(a>b) # Or
+print(8>5)
+print(8<5)
+print(8<=5)
+print(8>=5)
+print(8==8) #Is 8 equal to 8?
+print(8==5)
+print(a==b)
+print(a==10) #Is a equal to 10?
+
+print(a!=30) #Is a not equal 30?
