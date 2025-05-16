0e0ba939130f8048f2b5c2122cd6b50277a784bf
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2025-05-16 18:52:39 +0530

    Add files via upload

---
+++ 08_conditional/04_match_case.py
@@ -0,0 +1,15 @@
+a = int(input("Enter a number between 1 to 10 :  "))
+
+match a:
+    case 1:
+        print("You won a charger")
+    case 3:
+        print("You won $5 ")
+    case 6:
+        print("You won Camera")
+    case _:
+        print("Better Luck next time")
+    
+
+
+
