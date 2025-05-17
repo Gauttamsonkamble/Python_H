ce804bc4e0ef848895d3a7cce9144d2407ea9826
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2025-05-17 21:10:44 +0530

    Add files via upload

---
+++ 10_String/01_string.py
@@ -0,0 +1,10 @@
+
+name = "Gauttam"
+channelName = 'DeveloperCorners'
+
+position = '''Owner Of the channel ,
+and Python Trainer as well''' # tripple quates string is use for (multilines string) & comments as well
+
+print(name)
+print(channelName)
+print(position)
---
+++ 10_String/03_slicing.py
@@ -0,0 +1,20 @@
+
+name = "Gauttam"
+
+print(name[0:2]) # goes from 0 to 0-1 ie 0 to 1
+
+print(name[2:-1]) # same as name [2:6]
+
+name2 = "DeveloperCorners"
+
+print(len(name2))
+
+print(name2[0:10])
+
+print(name2[0:10:2]) # skip n-1 character ie here (2-1) skip character
+
+print(name2[0:10:3]) # skip 3-1 character means 2 character
+
+print(name2[:4]) # replace the first empty number with 0 # name[0:4]
+
+print(name2[1:]) # replace the second empty number with length # name[1:16]
