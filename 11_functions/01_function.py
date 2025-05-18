6be6d4767eaf73a58a5f0c1ec57b8cd96f89f034
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-05-18 20:53:21 +0530

    how to write function

---
+++ 11_functions/01_function.py
@@ -0,0 +1,25 @@
+
+# a = 10
+# b = 30
+# c = 20
+
+# avarage = (a+b+c)/3
+
+# print(avarage)
+
+def avarage(a,b,c):
+    d = (a+b+c)/3
+    print(d)
+
+avarage(10,30,20)
+avarage(2,6,4)
+
+# return keyword
+
+def avarage2(a,b,c):
+    d = (a+b+c)/3
+    return d
+
+avg = avarage2(3,9,5)
+
+print(avg)
