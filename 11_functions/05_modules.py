a2b632e7d1d8310ce4670a5dbc0c0ad6e26db881
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-05-21 22:31:19 +0530

    modules

---
+++ 11_functions/05_modules.py
@@ -0,0 +1,15 @@
+# Two types of modules in python:
+
+# 1. Build in modules ---> *Link ---> https://docs.python.org/3/py-modindex.html
+# 2. External Modules
+
+import math
+import os
+import mymodule
+
+
+
+print (math.sqrt(25))
+
+mymodule.hello()
+
