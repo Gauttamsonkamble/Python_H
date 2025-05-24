51bbbbd4baf331d2d6c48614e584820565f20f82
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-05-24 22:16:36 +0530

    Pthon Dictionary and it's methods

---
+++ 15_Dictionaries/01_dictionary.py
@@ -0,0 +1,11 @@
+
+marks = { "Gauttam":45, "Rober": 30,"jack":60,"Rhonda":70}
+
+print(marks)
+print(type(marks))
+
+print(marks["Gauttam"])
+
+marks["Gauttam"] = 90
+
+print(marks)
---
+++ 15_Dictionaries/02_dict_methods.py
@@ -0,0 +1,12 @@
+
+marks = { "Gauttam":45, "Robert": 30,"jack":60,"Rhonda":70}
+
+print(marks.keys())
+print(marks.values())
+
+marks.pop("Robert")
+print(marks)
+
+marks.clear()
+
+print(marks)
