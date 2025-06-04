f1c964d5ac62e6180e475fa30315f9d6d33b9f9c
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-06-04 22:35:25 +0530

    os and shutil module in python

---
+++ 18_files/06_os.py
@@ -0,0 +1,15 @@
+
+import os
+
+a = os.listdir("dir")
+
+print(a)
+
+print(os.getcwd())
+
+print(os.path.exists("dir"))
+print(os.path.exists("Gauttam"))
+
+# os.remove("demo.txt")
+
+os.rmdir("dir")
---
+++ 18_files/07_shutil.py
@@ -0,0 +1,8 @@
+
+import shutil
+
+# shutil.rmtree("dir")
+
+# shutil.copy("Gauttam.txt","jhon.txt")
+
+shutil.move("jhon.txt","dir")
