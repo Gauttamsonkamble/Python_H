465c96b8830ec40ce196e58f7be402d08b5e3661
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-06-06 21:42:21 +0530

    Virtual Environment in python

---
+++ 19_External Module/01_VEnv.py
@@ -0,0 +1,8 @@
+# v1 - package 1 (pandas)
+# v2 - package 2 (numpy)
+# v3 - package 3 (moviepy)
+#  ".\env2\Scripts\Activate.ps1" (To activate env2)
+
+# python -m venv DeveloperCorners  <---- Creating Virtual Environment
+
+import moviepy
